aaed0750a167531e1953e1ba6fcdf8b0e932a1b6
konstriktor74 <104011205+konstriktor74@users.noreply.github.com>
2022-11-25 00:44:05 -0300

    Agregar archivos de proyecto.

---
+++ UTNCursoApi/Controllers/v2/HealthCheck.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UTNCursoApi.Controllers.v2
+{
+    public class HealthCheck : Controller
+    {
+        [ApiVersion("2.0")]
+        [ApiController]
+        [Route("[controller]")]
+        public class HealthCheckController : ControllerBase
+        {
+            [HttpGet()]
+            public IActionResult Get()
+            {
+                return Ok("OK");
+            }
+
+        }
+    }
+}
